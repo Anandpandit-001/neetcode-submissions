5ee69df78b0db38ed19fae866860dce8eb93942b
Anand Pandit <anandpanditmvv@gmail.com>
2026-07-01 14:35:23 +0530

    Add: single-number - submission-3

---
+++ Data Structures & Algorithms/single-number/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        
+        # Simply XOR every single number into the running total
+        for val in nums:
+            result = result ^ val
+            
+        return result
